000f34ec53fa6420f44028c1e10f848aa25884aa
VDTC KTCN <>
2025-02-19 08:42:32 +0700

    init commit create project

---
+++ learningJavaStep/src/main/java/com/huytq/learningJavaStep/LearningJavaStepApplication.java
@@ -0,0 +1,13 @@
+package com.huytq.learningJavaStep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningJavaStepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningJavaStepApplication.class, args);
+	}
+
+}
---
+++ learningJavaStep/src/test/java/com/huytq/learningJavaStep/LearningJavaStepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huytq.learningJavaStep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningJavaStepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
